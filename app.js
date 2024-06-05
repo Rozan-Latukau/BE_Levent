2c07a8e569179e5e27f597b648b7b98bc41e5ed2
Rozan-Latukau <iyanlatu@gmail.com>
2024-06-05 20:40:29 +0700

    feat: install cors

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const cors = require("cors");
 
 const app = express();
 
@@ -22,6 +23,7 @@ const handleErrorMiddleware = require("./app/middlewares/handler-error");
 
 const v1 = "/api/v1";
 
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
